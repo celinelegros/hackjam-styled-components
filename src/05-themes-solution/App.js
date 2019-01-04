49988b32b06a18f5e5f2ee82112b1243c235df5a
Milenko <milenko@hackages.io>
2019-01-04 16:50:30 +0100

    Themes

--- src/05-themes-solution/App.js
+++ src/05-themes-solution/App.js
@@ -1,11 +1,48 @@
 import React, {Component} from 'react';
+import styled, {ThemeProvider} from 'styled-components'
+
+const lightTheme = {
+  primary: 'white',
+  text: 'black',
+};
+
+const darkTheme = {
+  primary: 'black',
+  text: 'white',
+};
 
 export class App extends Component {
-  render() {
-    return (
-      <div>
 
-      </div>
+  state = {
+    theme: 'light'
+  };
+
+  toggleTheme = () => this.setState({theme: this.state.theme === 'light' ? 'dark' : 'light'});
+
+  render() {
+    const {theme} = this.state;
+    return (
+      <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+        <Main toggleTheme={this.toggleTheme}/>
+      </ThemeProvider>
     );
   }
 }
+
+
+const StyledContainer = styled.div`
+  background: ${props => props.theme.primary};
+  height: 100vh;
+  width: 100%;
+`;
+
+const StyledTitle = styled.h2`
+  color: ${props => props.theme.text}
+`
+
+const Main = ({toggleTheme}) => (
+  <StyledContainer>
+    <StyledTitle>Hello There</StyledTitle>
+    <button onClick={toggleTheme}>Change theme</button>
+  </StyledContainer>
+)
--- src/05-themes/App.js
+++ src/05-themes/App.js
@@ -1,11 +1,44 @@
 import React, {Component} from 'react';
 
+
+// Create a lightTheme object and a darkTheme object
+// They both need to have a primary and a textColor property, which are just color in strings
+
+const ligthTheme = {
+
+}
+
+const darkTheme = {
+
+}
+
 export class App extends Component {
+
+  state = {
+    theme: 'light'
+  };
+
+  toggleTheme = () => this.setState({theme: this.state.theme === 'light' ? 'dark' : 'light'});
+
   render() {
+    const {theme} = this.state;
+
+    // Use a ThemeProvider and put the theme from the state
     return (
       <div>
-
+        <Main toggleTheme={this.toggleTheme}/>
       </div>
     );
   }
 }
+
+
+//This component will receive a function to change the theme
+//Create a styled div that will set the background to the primary from the background
+//Create a styled h2 that will set the color to the textColor from the theme
+const Main = () => {
+    return (
+      <div />
+    )
+}
+
