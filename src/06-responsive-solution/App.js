ace11c2340b60c8e2040f9aeec4956255b8c6061
celinelegros <celinelegros1993@gmail.com>
2019-01-04 16:48:26 +0100

    style(06-responsive + 06-responsive-solution):

--- src/06-responsive-solution/App.js
+++ src/06-responsive-solution/App.js
@@ -1,11 +1,36 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { media } from './mediaQuery';
+
+const ResponsiveContainer = styled.div`
+  width: calc(100% - 32px);
+  height: 400px;
+  margin: 16px auto;
+  position: relative;
+  background: blue;
+  h1 {
+    width: auto;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    text-align: center;
+    transform: translate(-50%, -50%);
+    color: white;
+  }
+  ${media.sm`
+  background : red;
+  h1{color: yellow;}`}
+  ${media.md`
+  background : violet;
+  h1{color: purple;}`}
+`;
 
 export class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <ResponsiveContainer>
+        <h1>Hello World, resize me ! </h1>
+      </ResponsiveContainer>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './04-animations/App';
+import { App } from './06-responsive-solution/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
